9701a2da03083730ef687346452ea23a489bd2b1
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2023-12-08 13:13:22 -0500

    commit 6

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,13 +1,17 @@
 const URL = "https://openlibrary.org/search.json";
 
-async function getData(URL){
-    try {
-        const response = await fetch(URL);
-        const data = await response.json();
-        document.getElementById("api-response").textContent = data.content;
-      console.log(data)
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    document.getElementById("api-response").textContent = data.content;
+    console.log(data);
+    if (response.status != 200) throw new Error(response.statusText);
+    document.querySelector("h1").textContent = data.content;
+    document.querySelector("h2").textContent = data.content; //change .content to match w my api
+    console.log(data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 getData(URL);
