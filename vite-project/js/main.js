584800f83d472749c606aadb67dc9b24533193d7
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2023-12-11 13:14:21 -0500

    commit 7

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -8,10 +8,24 @@ async function getData(URL) {
     console.log(data);
     if (response.status != 200) throw new Error(response.statusText);
     document.querySelector("h1").textContent = data.content;
-    document.querySelector("h2").textContent = data.content; //change .content to match w my api
+    document.querySelector("h2").textContent =
+      "Not found, please search for something else."; //change .content to match w my api
     console.log(data);
   } catch (error) {
     console.log(error);
   }
 }
 getData(URL);
+
+function greet(name) {
+  const greetPromise = new Promise(function (resolve, rejected) {
+    resolve(`Title: ${name}`);
+  });
+  return greetPromise;
+}
+
+const books = greet("books");
+console.log(books);
+books.then((result) => {
+  console.log(result);
+});
