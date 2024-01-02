2133e85b8392d046f257603bb9196a58f890124a
SpinlyBinly <jane.laduca@gmail.com>
2024-01-01 21:35:00 -0500

    commit 16

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,84 +1,43 @@
-let URL = `https://openlibrary.org/search.json`;
+const endpoint = `https://openlibrary.org/search.json`;
 
 const DOMSelectors = {
-  buttonTitle: document.querySelector("#btnTitle"),
-  buttonAuthor: document.querySelector("#btnAuthor"),
-  inputTitle: document.querySelector("#inptTitle"),
-  inputAuthor: document.querySelector("#inptAuthor"),
+  button: document.querySelector("#btn"),
+  input: document.querySelector("#input"),
+  container: document.querySelector("#container"),
 };
-
-async function getData(URL) {
-  try {
-    DOMSelectors.buttonTitle.addEventListener("click", async function (event) {
-      const response = await fetch(URL);
-      const data = await response.json();
-      event.preventDefault();
-      const value = DOMSelectors.inputTitle.value;
-      URL = `https://openlibrary.org/search.json?q=${value}&fields=key,title,author_name,editions`;
-      console.log(URL.replaceAll(" ", "+"));
-      const authors = authors.filter((author) => author.author_name.includes(inputAuthor));
-      clearFields();
-      losses.forEach((loss) => {
-      DOMSelectors.container.insertAdjacentHTML(
-        "beforeend",
-        `<div class=card><h1>${loss.firstName} ${loss.lastName}</h1>
-        <img class="imgs"src="${loss.img}" alt="">
-        <h3 id="h3" class="">${loss.description}</h3>
-        </div>`
-    });
-    DOMSelectors.buttonAuthor.addEventListener("click", async function (event) {
-      const response = await fetch(URL);
-      const data = await response.json();
-      event.preventDefault();
-      const value = DOMSelectors.inputTitle.value;
-      URL = `https://openlibrary.org/search.json?q=${value}&fields=key,title,author_name,editions`;
-      let x = document.URL.textContent;
-      console.log(URL.replaceAll(" ", "+"));
-      const authors = authors.filter((author) => author.author_name.includes(inputAuthor));
-      clearFields();
-      losses.forEach((loss) => {
-      DOMSelectors.container.insertAdjacentHTML(
-        "beforeend",
-        `<div class=card><h1>${loss.firstName} ${loss.lastName}</h1>
-        <img class="imgs"src="${loss.img}" alt="">
-        <h3 id="h3" class="">${loss.description}</h3>
-        </div>`
-    );
-  });
-});
-      const response = await fetch(URL);
-      const data = await response.json();
-      event.preventDefault();
-      const value = DOMSelectors.inputAuthor.value;
-      URL = `https://openlibrary.org/search.json?q=${value}&fields=key,title,author_name,editions`;
-      let x = document.URL.textContent;
-      console.log(URL.replaceAll(" ", "+"));
-};
-/*     if (response.status != 200) throw new Error(response.statusText);
-    document.querySelector("h1").textContent = data.docs;
-    document.querySelector("h2").textContent =
-      "Not found, please search for something else.";
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-}
-getData(URL);
-
-function greet(title) {
-  const greetPromise = new Promise(function (resolve, rejected) {
-    resolve(`Title: ${title}`);
-  });
-  return greetPromise;
-}
-
-const books = greet("books");
-console.log(books);
-books.then((result) => {
-  console.log(result);
-});
 
 function clearFields() {
   DOMSelectors.container.innerHTML = "";
-}
- */
+};
+
+async function getData(apiEndpoint) {
+  try {
+    DOMSelectors.button.addEventListener("click", async function (event) {
+      event.preventDefault();
+      const response = await fetch(apiEndpoint);
+      const data = await response.json();
+      const value = DOMSelectors.input.value;
+      const URL = `https://openlibrary.org/search.json?q=${value}&fields=key,title,author_name,editions`;
+      console.log(URL.replaceAll(" ", "+"));
+      clearFields();
+      data.docs.forEach((book) => {
+      DOMSelectors.container.insertAdjacentHTML(
+        "beforeend",
+        `<div class=card><h1>${book.title}</h1>
+        <img class="imgs"src="${book.img}" alt="">
+        <h3 id="h3" class="">${book.author}</h3>
+        </div>`
+    );
+  });
+    });
+    
+         if (response.status != 200) throw new Error(response.statusText);
+        document.querySelector("h1").textContent = data.docs;
+        document.querySelector("h2").textContent = "Not found, please search for something else.";
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+getData(endpoint);
+
