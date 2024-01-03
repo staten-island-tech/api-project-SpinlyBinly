c21bef8eb9e5aba0b810f3e2e6b9115efcbddc6f
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2024-01-03 13:15:05 -0500

    commit 19

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -6,43 +6,48 @@ const DOMSelectors = {
 
 function clearFields() {
   DOMSelectors.container.innerHTML = "";
-}   
+}
 async function getData() {
-/*   try { */
+  try {
     DOMSelectors.button.addEventListener("click", async function (event) {
       event.preventDefault();
       const values = DOMSelectors.input.value;
-      const response = await fetch(`https://openlibrary.org/search.json?q=${values}&fields=key,title,author_name,editions`);
-      /* if (!response.ok) {
+      const response = await fetch(
+        `https://openlibrary.org/search.json?q=${values}&fields=key,title,author_name,editions`
+      );
+      if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
-      } */
+      }
       const data = await response.json();
-      clearFields()
-/*       if (data.docs.length === 0) { */
+      clearFields();
+      if (data.docs.length === 0) {
         DOMSelectors.container.insertAdjacentHTML(
           "beforeend",
           `<div class="card">
             <h2>No results found. Please try another search.</h2>
           </div>`
         );
-      } /* else {
+      } else {
         data.docs.forEach((value) => {
-          
-          const authors = value.author_name ? value.author_name.join(', ') : 'Unknown Author';
- */
+          const authors = value.author_name;
+          let images = value.key;
+          images = images.replace("/works/", "");
+
+          console.log(images);
           DOMSelectors.container.insertAdjacentHTML(
             "beforeend",
             `<div class="card">
               <h1>${value.title}</h1>
+              <img src="https://covers.openlibrary.org/a/olid/OL23919A-M.jpg" alt="Book Covers">
               <h3 id="h3" class="">${authors}</h3>
             </div>`
           );
-        }/* );
+        });
       }
     });
   } catch (error) {
     console.error(error);
   }
 }
- */
+
 getData();
