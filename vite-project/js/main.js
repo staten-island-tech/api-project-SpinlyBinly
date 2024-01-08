25b7489742e47bb241a21c89fa38302848257164
SpinlyBinly <jane.laduca@gmail.com>
2024-01-08 00:33:15 -0500

    commit 23

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -8,47 +8,52 @@ function clearFields() {
   DOMSelectors.container.innerHTML = "";
 }
 
-async function getData() {
-  try {
-    DOMSelectors.button.addEventListener("click", async function (event) {
-      event.preventDefault();
-      const values = DOMSelectors.input.value;
-      const response = await fetch(
-        `https://openlibrary.org/search.json?q=${values}&fields=key,title,author_name,editions`
-      );
-      const data = await response.json();
-      clearFields();
-
-      if (data.docs.length === 0) {
-        DOMSelectors.container.insertAdjacentHTML(
-          "beforeend",
-          `<h2>No results found. Please try another search.</h2>`
+  async function getData() {
+    try {
+      DOMSelectors.button.addEventListener("click", async function (event) {
+        event.preventDefault();
+        const values = DOMSelectors.input.value;
+        const response = await fetch(
+          `https://openlibrary.org/search.json?q=${values}&fields=key,title,author_name,editions`
         );
-      } else {
-        data.docs.forEach((value) => {
-          const authors = value.author_name;
-          console.log(value.editions.docs[0].key);
-          let images = value.editions.docs[0].key.split("/");
-          /*           let imag = images.replace("/works/", ""); */
-          /*   imag = imag.replace("W", ""); */
-          //console.log(`https://covers.openlibrary.org/b/olid/${images}-S.jpg`);
+        const data = await response.json();
+        clearFields();
 
+        if (data.docs.length === 0) {
           DOMSelectors.container.insertAdjacentHTML(
             "beforeend",
-            `<div class="card">
-              <h1>${value.title}</h1>
-              <img src="https://covers.openlibrary.org/b/olid/${images[2]}-M.jpg" alt="Book Covers" class=image>
-              <h3 id="h3" class="">${authors}</h3>
-            </div>`
+            `<h2>No results found. Please try another search.</h2>`
           );
-        });
-      }
-    });
-  } catch (error) {
-    console.error(error);
+        } else {
+          data.docs.forEach((value) => {
+            const authors = value.author_name;
+            let images = value.editions.docs[0].key.split("/");
+            DOMSelectors.container.insertAdjacentHTML(
+              "beforeend",
+              `<div class="card">
+              <h2 tabindex=0>${value.title}</h2>
+              <img tabindex=1 src="https://covers.openlibrary.org/b/olid/${images[2]}-L.jpg" alt="${value.title} Cover" class=image>
+              <h2 tabindex=2 id="h2" class="">${authors}</h2>
+              </div>`
+            );
+            function greet(name) {
+              const greetPromise = new Promise(function (resolve, rejected) {
+                resolve(`Title: ${value.title}`);
+              });
+              return greetPromise;
+            }
+
+            const books = greet("books");
+            console.log(books);
+            books.then((result) => {
+              console.log(result);
+            });
+          });
+        }
+      });
+    } catch (error) {
+      console.error(error);
+    }
   }
-}
 
-getData();
-
-//"/books/OL27296256M"
+  getData();
